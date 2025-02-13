3ddc2e39b6fefceeaffce2bd17624566c908911b
javier <javier.rodriguez@dev-codes.io>
2025-02-12 21:45:37 -0500

    ADD: test examples

--- src/service/ListMoviesService.java
+++ src/service/ListMoviesService.java
@@ -16,7 +16,8 @@ public class ListMoviesService implements IServiceMovies {
     @Override
     public void listMovies() {
         System.out.println("Listado de peliculas...");
-        movies.forEach(System.out::println);
+        //movies.forEach(System.out::println);
+        movies.forEach((movie)-> System.out.println(movie.getName()));
     }
 
     @Override
@@ -29,6 +30,29 @@ public class ListMoviesService implements IServiceMovies {
     public void searchMovie(Pelicula pelicula) {
         //Regresa el indice de la pelicula encontrada en la lista
         var index = movies.indexOf(pelicula);
-        System.out.println("Pelicula encontrada en el indice: " + index);
+        if(index == -1){
+            System.out.println("No se encontro la pelicula: " + pelicula);
+        }else {
+            System.out.println("Pelicula: " + pelicula.getName() + " encontrada en el indice: " + index);
+        }
+    }
+
+    public static void main(String[] args) {
+        Pelicula pelicula_1 = new Pelicula("Batman");
+        Pelicula pelicula_2 = new Pelicula("Superman");
+
+        // Implementar el servicio, izquierda la interface, derecha el servicio
+        IServiceMovies moviesService = new ListMoviesService();
+
+        // Agregar peliculas al servicio
+        moviesService.addMovie(pelicula_1);
+        moviesService.addMovie(pelicula_2);
+
+        //listar peliculas
+        moviesService.listMovies();
+
+        //buscar peliculas
+        moviesService.searchMovie(pelicula_1);
+        moviesService.searchMovie(new Pelicula("Transformes"));
     }
 }
